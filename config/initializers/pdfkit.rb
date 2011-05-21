a3a4f481b4aa600a7dbbc13b8dacdfd45b7076ed
Peter Casanova <peter.casanova@comcast.net>
2011-05-21 17:39:21 -0400

    testing pdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,12 @@
-# PDFKit.configure do |config|
-#   config.default_options = {
-#     :page_size => 'Letter',
-#     :print_media_type => true,
-#     :disable_javascript => true
-#   }
-# end
+PDFKit.configure do |config|
+  config.wkhtmltopdf = `which wkhtmltopdf`.to_s.strip
+  config.default_options = {
+    :encoding=>"UTF-8",
+    :page_size=>"A4",
+    :margin_top=>"0.5in",
+    :margin_right=>"0.75in",
+    :margin_bottom=>"0.5in",
+    :margin_left=>"0.75in",
+    :disable_smart_shrinking=>false
+    }
+end
