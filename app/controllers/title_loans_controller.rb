a7afdc8125022a599b945b969123a37886da3bfd
Peter Casanova <peter.casanova@comcast.net>
2011-05-21 17:49:31 -0400

    testing pdf

--- app/controllers/title_loans_controller.rb
+++ app/controllers/title_loans_controller.rb
@@ -1,6 +1,6 @@
 class TitleLoansController < ApplicationController
-  load_and_authorize_resource
-  before_filter :authenticate_user!
+  # load_and_authorize_resource
+  # before_filter :authenticate_user!
   
   def index
     @title_loans = TitleLoan.all
